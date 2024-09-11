69bb7091b4857c0ef874191bc62902bbf7c3104e
Lucastdeg <lucastdeg@gmail.com>
2024-09-11 17:34:47 -0500

    Added nicla vision code

--- src/NIcla Code/main.py
+++ src/NIcla Code/main.py
@@ -1,144 +1,96 @@
-# Untitled - By: lucas - Thu Jul 11 2024
-
 import sensor, image, time
-import pyb # Import module for board related functions
+import pyb  # Import module for board-related functions
 from pyb import I2C
 import utime
-import machine
 
 def main():
-
     pyb.usb_mode("CDC")  # Disable USB mode to avoid waiting for a connection
-
     utime.sleep_ms(2000)
-    #i2c = I2C(2)                         # create on bus 2
-    i2c = I2C(1, I2C.MASTER)             # create and init as a master
-    i2c.init(I2C.MASTER, baudrate=100000) # init as a master
 
-    # And a short delay to wait until the I2C port has finished activating.
+    # Initialize I2C in slave mode on bus 1 with address 0x08
+    i2c = I2C(1, I2C.SLAVE, addr=0x08, baudrate=100000)
 
-    redLED = pyb.LED(1) # built-in red LED
-    greenLED = pyb.LED(2) # built-in green LED
-    blueLED = pyb.LED(3) # built-in blue LED
+    # LEDs for status indication
+    redLED = pyb.LED(1)  # built-in red LED
+    greenLED = pyb.LED(2)  # built-in green LED
+    blueLED = pyb.LED(3)
 
     def sendInfo(value):
-        # Convert the values to a byte array
-        #data_to_send = bytearray([value, color])
         data_to_send = bytearray([value])
-
         try:
-            # Send the data to the Arduino Nano Every (slave address 0x08)
-            i2c.send(data_to_send, addr=0x08)
+            i2c.send(data_to_send)  # Send the data to the master
             print("Data sent:", data_to_send)
         except OSError as e:
             print("I2C Error:", e)
+            redLED.on()  # Indicate error with red LED
+            time.sleep(0.5)
+            redLED.off()
 
-        time.sleep(1)  # Wait for a second before sending the next data
+        time.sleep(1)  # Wait for a second before the next operation
 
     def cameraConfig(formats=sensor.RGB565, resolution=sensor.QVGA):
-        sensor.reset() # Initialize the camera sensor.
-        sensor.set_pixformat(formats) # Sets the sensor to RGB
-        sensor.set_framesize(resolution) # Sets the resolution to 320x240 px
-        #https://en.wikipedia.org/wiki/Display_resolution_standards
+        sensor.reset()  # Initialize the camera sensor.
+        sensor.set_pixformat(formats)  # Sets the sensor to RGB
+        sensor.set_framesize(resolution)  # Sets the resolution to 320x240 px
+        sensor.set_brightness(3)
 
-        def takePicture(flip=False, mirror=False, frames=200, msg=""):
+        def takePicture(flip=False, mirror=False, frames=200):
             sensor.set_vflip(flip)
             sensor.set_hmirror(mirror)
-
-            #redLED.on()
             sensor.skip_frames(time=frames)
-            #redLED.off()
-            #blueLED.on()
-
             img = sensor.snapshot()
-
-            #blueLED.off()
-
             return img
 
         return takePicture
 
-    def get_biggest_color(red, green, blue):
-        if red > green and red > blue and red < 70 and red > 20:
-            return 1, 0
-        elif green > red and green > blue and green < 70 and green > 20:
-            return 0, 1
+    def find_color_blobs(img):
+        # Define color thresholds for broader sensitivity
+        red_threshold = (10, 100, 10, 127, 0, 127)  # Expanded red range
+        green_threshold = (15, 100, -80, -8, -60, 60)  # Expanded green range
+
+        red_blobs = img.find_blobs([red_threshold], pixels_threshold=18, area_threshold=18, merge=True)
+        green_blobs = img.find_blobs([green_threshold], pixels_threshold=18, area_threshold=18, merge=True)
+
+        red_pixel_count = sum(blob.pixels() for blob in red_blobs)
+        green_pixel_count = sum(blob.pixels() for blob in green_blobs)
+
+        print("Red pixels:", red_pixel_count, "Green pixels:", green_pixel_count)
+
+        if red_pixel_count > green_pixel_count and red_pixel_count > 100:
+            return 1  # Red detected
+        elif green_pixel_count > red_pixel_count and green_pixel_count > 100:
+            return 0  # Green detected
         else:
-            return 0, 0
+            return -1  # No significant blobs detected
 
-    def calculate_average_color(img, roi):
-        x, y, w, h = roi
-        r_total, g_total, b_total = 0, 0, 0
-        pixel_count = w * h
-        for i in range(x, x + w):
-            for j in range(y, y + h):
-                r, g, b = img.get_pixel(i, j)
-                r_total += r
-                g_total += g
-                b_total += b
+    blueLED.on()  # Turn on red LED to indicate the device is ready
 
-        r_avg = r_total // pixel_count
-        g_avg = g_total // pixel_count
-        b_avg = b_total // pixel_count
-
-        return (r_avg, g_avg, b_avg)
-
-    def get_consecutives(img):
-        r_p_t, g_p_t = 0, 0
-        r_pixel_count, g_pixel_count = 0, 0
-        consecutive_red = 0
-        consecutive_green = 0
-        startr = True
-        startg = True
-
-        for i in range(1, 320):
-            for j in range(1, 240):
-                r, g, b = img.get_pixel(i, j)
-                r_p_t, g_p_t = get_biggest_color(r, g, b)
-                if r_p_t:
-                    consecutive_red += 1
-                elif g_p_t:
-                    consecutive_green += 1
-                else:
-                    consecutive_red = 0
-                    consecutive_green = 0
-
-                if consecutive_red > 10:
-                    if startr:
-                        r_pixel_count = 11
-                        startr = False
-                    else:
-                        r_pixel_count += 1
-                if consecutive_green > 10:
-                    if startg:
-                        g_pixel_count = 11
-                        startg = False
-                    else:
-                        g_pixel_count += 1
-
-        return r_pixel_count, g_pixel_count
-
-    while(True):
+    while True:
         TakePic = cameraConfig()
         img = TakePic()
-        r, g, b = calculate_average_color(img, (1, 1, 319, 239))
-        #con_red, con_green = get_consecutives(img)
-        #print(get_consecutives(img))
-        #print(r, g, b)
 
-        if r > g:
-            print("RED")
+        color_detected = find_color_blobs(img)
+
+        if color_detected == 1:  # Red detected
+            print("RED detected")
             greenLED.off()
             redLED.on()
             sendInfo(1)
-        elif g > r:
-            print("GREEN")
+        elif color_detected == 0:  # Green detected
+            print("GREEN detected")
             redLED.off()
             greenLED.on()
             sendInfo(0)
-        else:
-            print("NEITHER")
+        else:  # Neither detected
+            print("NEITHER detected")
+            redLED.off()
+            greenLED.off()
+            blueLED.on()
+            sendInfo(255)  # Send 255 as a signal for "neither"
+
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
+
 
